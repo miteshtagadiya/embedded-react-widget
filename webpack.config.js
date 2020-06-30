845f5adad261441b06efd4e3c29ba20f08c45665
mitesh <miteshtagadiya@gmail.com>
2020-06-30 23:33:32 +0530

    add webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,89 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const increaseSpecificity = require('postcss-increase-specificity');
+const JavaScriptObfuscator = require('webpack-obfuscator');
+const CopyPlugin = require('copy-webpack-plugin');
+const path = require('path');
+
+const devMode = process.env.NODE_ENV !== 'production';
+
+const publicDir = path.join(__dirname, 'public');
+const distDir = path.join(__dirname, 'dist');
+
+const defaultConfig = {
+  mode: process.env.NODE_ENV || 'development',
+  devServer: {
+    contentBase: publicDir,
+    port: 9000,
+  },
+  plugins: [
+    // new CleanWebpackPlugin({protectWebpackAssets: false}),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: devMode ? '[name].css' : '[name].[hash].css',
+      chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+    }),
+    new CopyPlugin([
+      { from: 'public', to: '.' },
+    ]),
+    devMode ? null : new JavaScriptObfuscator(),
+  ].filter((i) => i),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+        options: {
+          emitWarning: true,
+        },
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          // fallback to style-loader in development
+          // devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'style-loader',
+          'css-loader',
+          'cssimportant-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: [
+                increaseSpecificity({
+                  stackableRoot: '.cleanslate',
+                  repeat: 1,
+                }),
+              ],
+              sourceMap: devMode,
+            },
+          },
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+};
+
+module.exports = [{
+  ...defaultConfig,
+  entry: './src/components/embeddable-widget.js',
+  output: {
+    path: distDir,
+    publicPath: '/',
+    filename: 'widget.js',
+    library: 'EmbeddableWidget',
+    libraryExport: 'default',
+    libraryTarget: 'window',
+  },
+}];
